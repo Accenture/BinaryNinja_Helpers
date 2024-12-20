a72e362af6a863a834d4da1ae2e18935042d30b0
Martin Petran <martin.petran@pm.me>
2024-12-20 14:03:41 +0100

    ff improved

--- sidekick/function_finder/README.md
+++ sidekick/function_finder/README.md
@@ -1,3 +1,4 @@
 # Functions Finder
 
 Script to find XREFs to current functions that may be hidden in the code that was not discovered during the initial analysis. Usually not necessary but sometimes useful. Sidekick index will be created with all spots that are not functions but contain instructions that XREF the current function. This is helpful with logging functions which are called very often.
+Furthermore, the script will also automatically search for the occurrence of the address of the function within the selected segment. This is something you can do with simple byte search, but this way it will organize the references in same index for better readability.
--- sidekick/function_finder/function_finder.py
+++ sidekick/function_finder/function_finder.py
@@ -10,18 +10,25 @@ if segment_choice != None:
     start_addr = segments_list[segment_choice][0]
     end_addr = segments_list[segment_choice][1]
     total_calls = 0
+    addr_refs = 0
     with open_index(bv, f"Undiscovered XREFs to {target_func_name}") as index:
+        for raw_ptr in bv.find_all_data(start_addr,end_addr,target_func_addr.to_bytes(bv.arch.address_size, "little" if bv.arch.endianness == Endianness.LittleEndian else "big")):
+            if not bv.get_data_var_at(raw_ptr[0]):
+                bv.define_user_data_var(raw_ptr[0],"void*")
+            index.add_entry(bv.get_data_var_at(raw_ptr[0]),{"XREF Type": "Raw pointer address"})
+            addr_refs += 1
         for current_address in range(start_addr,end_addr,bv.arch.instr_alignment):
             notify_progress(current_address - start_addr, end_addr - start_addr, 'Trying to find unknown XREFs')
             if not bv.get_functions_containing(current_address):
-                data = bv.read(current_address,bv.arch.max_instr_length)
+                data = bv.read(current_address,bv.arch.max_instr_length if bv.arch.max_instr_length >= 4 else 4)
                 instruction = bv.arch.get_instruction_info(data,current_address)
                 if instruction:
                     for branch in instruction.branches:
                         if branch.target == target_func_addr:
                             bv.define_user_data_var(current_address,"uint32_t")
-                            index.add_entry(bv.get_data_var_at(current_address),{"Potential XREF at": hex(current_address)})
+                            index.add_entry(bv.get_data_var_at(current_address),{"XREF Type": "Call instruction"})
                             #log_info(f"GOT CALL TO FUNC THAT IS NOT RECOGNIZED @ {hex(current_address)}")
                             total_calls += 1
 
-    print(f"Total new calls discovered: {total_calls}")
+    print(f"New calls discovered: {total_calls}")
+    print(f"Direct address refrences found: {addr_refs}")
